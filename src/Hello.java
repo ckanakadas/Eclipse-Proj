58730dacb02e2e7ab7ce5bf39603d4c6204d756c
ckanakadas <ckanakadas@csc.com>
2016-01-21 14:26:07 +0530

    Hello World

--- src/Hello.java
+++ src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 			//This is my first java project 
 			System.out.println(" My first java project");
 			
-		    System.out.println("Hello baby");
+		    System.out.println("Hello World");
 		
 		
 
