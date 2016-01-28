1c89e53ec1188e732bae25d102cb73a83277caae
ckanakadas <chaitraDK9738@gmail.com>
2016-01-28 14:56:07 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		public static void main(String[] args) {
 			
 			//This is my first java project 
+			//welcome
 			System.out.println(" My first java project");
 			
 		    System.out.println("Hello World");
