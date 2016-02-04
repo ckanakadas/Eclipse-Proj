ff3bc6ca8b5c8333d17830c09d475d0a96bfb3df
ckanakadas <chaitraDK9738@gmail.com>
2016-02-04 15:52:23 +0530

    much

--- src/Hello.java
+++ src/Hello.java
@@ -6,12 +6,9 @@ public class Hello {
 		public static void main(String[] args) {
 			
 			//This is my first java project 
-			//welcome
 			System.out.println(" My first java project");
 			
 		    System.out.println("Hello World");
-
-              System.out.println("Hello World2");
 		
 		
 
